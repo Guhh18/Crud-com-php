950e280951936d269178135cf0f870269cac467b
Gustavo Oliveira <gustavoplay381@gmail.com>
2023-05-05 11:50:53 -0300

    pesquisa funcionando

---
+++ bd_cadastro.sql
@@ -0,0 +1,50 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Tempo de geração: 27-Abr-2023 às 20:34
+-- Versão do servidor: 8.0.31
+-- versão do PHP: 8.0.26
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `bd_cadastro`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `tb_login`
+--
+
+DROP TABLE IF EXISTS `tb_login`;
+CREATE TABLE IF NOT EXISTS `tb_login` (
+  `id_conta` int NOT NULL AUTO_INCREMENT,
+  `usuario` varchar(60) NOT NULL,
+  `email` varchar(60) NOT NULL,
+  `senha` varchar(20) NOT NULL,
+  PRIMARY KEY (`id_conta`)
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Extraindo dados da tabela `tb_login`
+--
+
+INSERT INTO `tb_login` (`id_conta`, `usuario`, `email`, `senha`) VALUES
+(5, 'Gustavo', 'Gustavo@gmail.com', '123'),
+(4, 'Cleitin', 'cleitin2@gmail.com', '4321');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- sistema.php
+++ sistema.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+include_once('conexao.php');
 
 if ((!isset($_SESSION['email']) == true) and (!isset($_SESSION['senha']) == true)) {
 
@@ -10,7 +11,19 @@ if ((!isset($_SESSION['email']) == true) and (!isset($_SESSION['senha']) == true
 
 $logado = $_SESSION['email'];
 
+
+
+if (isset($_POST['busca_nome']) != '') {
+    $sql = "SELECT * FROM `tb_login` WHERE usuario LIKE '{$_POST['busca_nome']}%' ORDER BY usuario ASC";
+
+    $result = $conectaBanco->query($sql);
+} else {
+    $sql = "SELECT * FROM `tb_login` ORDER BY usuario ASC";
+
+    $result = $conectaBanco->query($sql);
+}
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -19,14 +32,70 @@ $logado = $_SESSION['email'];
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sistema</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 
 <body>
-    <h1>Vc acessou o sistema</h1>
-    <?php
-        echo "<h1>Bem vindo $logado</h1>";
-    ?>
-    <a href="sair.php">Sair</a>
+    
+    <header>
+        <a href="sair.php" class="sair">Sair</a>
+    </header>
+
+    <main class="container">
+        <form name="form1" action="sistema.php" method="POST">
+            <input type="text" name="busca_nome">
+            <input type="submit" value="Filtrar">
+        </form>
+
+        <h1>Listagem de contas</h1>
+        <table border="1" align="center">
+            <thead>
+                <tr>
+                    <th>Usuário</th>
+                    <th>E-mail</th>
+                    <th>Senha</th>
+                    <th>Editar</th>
+                    <th>Excluir</th>
+                </tr>
+            </thead>
+
+            <tbody>
+                <?php
+                while ($linha = mysqli_fetch_assoc($result)) {
+                    echo "<tr>";
+                    echo "<td>" . $linha['usuario'] . "</td>";
+                    echo "<td>" . $linha['email'] . "</td>";
+                    echo "<td>" . $linha['senha'] . "</td>";
+                    echo
+                    "<td align='center'>
+                        <a href='#'>
+                            <img src='images/edit-regular-24.png'></img>
+                        </a>
+                    </td>";
+                    echo
+                    "<td align='center'>
+                        <a href='#'>
+                            <img src='images/folder-minus-solid-24.png'></img>
+                        </a>
+                    </td>";
+                }
+                ?>
+            </tbody>
+
+        </table>
+    </main>
+    
+
+
+
+
+
+
+    <!-- <h1>Vc acessou o sistema</h1>
+
+        echo "<h1>Bem vindo $logado</h1>";-->
+
+
 </body>
 
 </html>
